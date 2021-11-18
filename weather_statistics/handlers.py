588d461f940cd49c8c35049a63097a581011417f
capitanFlint129 <47683361+capitanFlint129@users.noreply.github.com>
2021-11-18 18:16:40 +0300

    Update handlers.py

--- weather_statistics/handlers.py
+++ weather_statistics/handlers.py
@@ -38,6 +38,7 @@ def get_city_data_for_last_days():
         )
     date_fromat = '%Y-%m-%d'
     response_data = {
+        "devOpsSchool": "done",
         "city": city,
         "from": start_date.strftime(date_fromat),
         "to": end_date.strftime(date_fromat),
